461f8e5d6c0fa43b706d28ff72cccddc968464dd
athulpranav <athulpranav01@gmail.com>
2022-06-25 01:19:44 -0700

    done

---
+++ contactus.js
@@ -0,0 +1,12 @@
+contactbtn=()=>{
+    NAME=(document.getElementById("name").value)
+    MESSEGE=(document.getElementById("messege").value)
+    PHONE=(document.getElementById("phone").value)
+     console.log(NAME)
+     alert(NAME)
+    
+     console.log(MESSEGE)
+     alert(MESSEGE)
+     console.log(PHONE)
+     alert(PHONE)
+}
